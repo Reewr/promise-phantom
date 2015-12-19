ebfdf658a76f971c12e64eb7bd7e9977708525fc
Alexander Imenes <alexander.imenes@gmail.com>
2015-12-19 11:40:17 +0100

    Removed NYI function and updated examples with comments

--- examples/example-co.js
+++ examples/example-co.js
@@ -1,15 +1,24 @@
-// Simple example of how to use this library using Co, es6 syntax
+// Simple example of how to use this library using Co
 const co = require('co');
-const phantomjs = require('phantomjs-promise');
+const phantomjs = require('promise-phantom');
 
 let generatorFn = function* () {
+  // First, by creating a phantom instance,
+  // you are starting up phantomJS in a different process.
+  // the 'phantom'-object will be what you will be using
   let phantom = yield phantomjs.create();
+
+  // Creating a page is as simple as this,
+  // and can be done several times for all the pages you need.
+  // Make sure to run page.close() or phantom.exit()
+  // when you are done.
   let page    = yield phantom.createPage();
 
-  yield page.set('viewportSize', {width: 1024, height: 512});
-  yield page.open('http://www.google.com');
-  yield page.render('google.pdf');
-  yield phantom.exit();
+  yield page.set('viewportSize', {width: 1024, height: 512}); // Set the viewport size
+  yield page.open('http://www.google.com'); // then open google 
+  yield page.render('google.pdf'); // then render google to a pdf
+  yield phantom.exit(); // and lastly, exiting the phantom process, clearing memory in phantomjs
 };
 
+// Run the generatorFunction - returns a promise
 co(generatorFn);
--- examples/example.js
+++ examples/example.js
@@ -1,12 +1,21 @@
-// Simple example of how to use this library, es6 syntax
-const phantomjs = require('phantomjs-promise');
+// Simple example of how to use this library
+const driver = require('promise-phantom');
 
-phantomjs.create().then((phantom) => {
+// First, by creating a phantom instance,
+// you are starting up phantomJS in a different process.
+// the 'phantom'-object will be what you will be using
+driver.create().then((phantom) => {
+
+  // Creating a page is as simple as this,
+  // and can be done several times for all the pages you need.
+  // Make sure to run page.close() or phantom.exit()
+  // when you are done.
   phantom.createPage().then((page) => {
+    // Set the viewport to a specific size
     page.set('viewportSize', {width: 1024, height: 512})
-      .then(() => page.open('http://www.google.com'))
-      .then(() => page.render('google.pdf'))
-      .then(() => phantom.exit())
+      .then(() => page.open('http://www.google.com')) // Open google
+      .then(() => page.render('google.pdf')) // render google to a pdf
+      .then(() => phantom.exit()) // and lastly exit
       .catch((err) => console.error(err));
   });
 }).catch((err) => {
