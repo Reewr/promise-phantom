e906380bce0f76a654fc23f732399b84bcb0dda3
Reewr <alexander.imenes@gmail.com>
2016-08-13 01:34:20 +0200

    Fixes params and returns to be more consistent
    This fixes an issue where the documentation would be inconsistent in
    return types and parameter types.

--- index.js
+++ index.js
@@ -30,22 +30,22 @@ module.exports = {
    * `disk-cache`                  | true, false, yes, no     | Enables disk cache (at desktop services cache storage location, default is false).
    * `ignore-ssl-errors`           | true, false, yes, no     | Ignores SSL errors, such as expired or self-signed certificate errors (default is false).
    * `load-images`                 | true, false, yes, no     | Load all inlined images (default is true).
-   * `local-storage-path`          | string                   | Path to save LocalStorage content and WebSQL content.
-   * `local-storage-quota`         | number                   | Number maximum size to allow for data.
+   * `local-storage-path`          | String                   | Path to save LocalStorage content and WebSQL content.
+   * `local-storage-quota`         | Number                   | Number maximum size to allow for data.
    * `local-to-remote-url-access`  | true, false, yes, no     | Allows local content to access remote URL (default is false).
-   * `max-disk-cache-size`         | number                   | Size limits the size of disk cache (in KB).
-   * `output-encoding`             | string                   | Encoding sets the encoding used for terminal output (default is utf8).
-   * `remote-debugger-port`        | number                   | Starts the script in a debug harness and listens on the specified port
-   * `remote-debugger-autorun`     | string                   | Runs the script in the debugger immediately: `yes` or `no` (default)
-   * `proxy=address`               | string                   | Specifies the proxy server to use (e.g. --proxy=192.168.1.42:8080).
+   * `max-disk-cache-size`         | Number                   | Size limits the size of disk cache (in KB).
+   * `output-encoding`             | String                   | Encoding sets the encoding used for terminal output (default is utf8).
+   * `remote-debugger-port`        | Number                   | Starts the script in a debug harness and listens on the specified port
+   * `remote-debugger-autorun`     | String                   | Runs the script in the debugger immediately: `yes` or `no` (default)
+   * `proxy=address`               | String                   | Specifies the proxy server to use (e.g. --proxy=192.168.1.42:8080).
    * `proxy-type`                  | http, socks5, none       | Specifies the type of the proxy server (default is http).
-   * `proxy-auth`                  | string                   | Specifies the authentication information for the proxy, e.g. --proxy-auth=username:password).
-   * `script-encoding`             | string                   | Encoding sets the encoding used for the starting script (default is utf8).
+   * `proxy-auth`                  | String                   | Specifies the authentication information for the proxy, e.g. --proxy-auth=username:password).
+   * `script-encoding`             | String                   | Encoding sets the encoding used for the starting script (default is utf8).
    * `ssl-protocol`                | sslv3, sslv2, tlsv1, any | Sets the SSL protocol for secure connections (default is SSLv3).
    * `ssl-certificates-path`       | value                    | Sets the location for custom CA certificates (if none set, uses system default).
    * `web-security`                | true, false, yes, no     | Enables web security and forbids cross-domain XHR (default is true).
-   * `webdriver`                   | string                   | Starts in `Remote WebDriver mode` (embedded GhostDriver): `[[:]]` (default `127.0.0.1:8910`)
-   * `webdriver-selenium-grid-hub` | string                   | URL to the Selenium Grid HUB: `URLTOHUB` (default `none`) (NOTE: works only together with `--webdriver`)
+   * `webdriver`                   | String                   | Starts in `Remote WebDriver mode` (embedded GhostDriver): `[[:]]` (default `127.0.0.1:8910`)
+   * `webdriver-selenium-grid-hub` | String                   | URL to the Selenium Grid HUB: `URLTOHUB` (default `none`) (NOTE: works only together with `--webdriver`)
    *
    * As of version 2.2.3 of node-phantom-simple, the parameter option may also
    * be an array of verbatim keys and values.name
@@ -53,11 +53,11 @@ module.exports = {
    * See [Phantom]{@link ./phantom.md} for more info regarding the
    * promised Phantom-object
    *
-   * @param {object} options
-   * @param {string} options.path Path to phantomJS/SlimerJS.
+   * @param {Object} options
+   * @param {String} options.path Path to phantomJS/SlimerJS.
    *                              If this is not set, node-phantom-simple
    *                              will search $PATH
-   * @param {object} options.parameters An object of key/value of options for PhantomJS
+   * @param {Object} options.parameters An object of key/value of options for PhantomJS
    * @promise {Phantom} A wrapper around the Browser object
    * @example
    * var driver = require('promise-phantom');
